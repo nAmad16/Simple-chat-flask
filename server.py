26c069ba0f5a7a6934d54fe3a4a7df6fe2a7d320
mrcRonny <131894082+mrcRonny@users.noreply.github.com>
2025-08-13 02:47:01 +0400

    Update server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+@app.route('/')
+def home():
+    return "Chat server is running!"
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message received: {msg}")
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # Render'ın PORT değişkenini kullan
+    socketio.run(app, host='0.0.0.0', port=port)
