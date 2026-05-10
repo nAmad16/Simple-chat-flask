273fafc5c258c0452313282a46abe2373cee8b46
Nihad <131894082+nAmad16@users.noreply.github.com>
2026-05-11 00:19:07 +0400

    Fix indentation in server.py

--- server.py
+++ server.py
@@ -16,5 +16,5 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Render'ın PORT değişkenini kullan
+    port = int(os.environ.get('PORT', 10000))
     socketio.run(app, host='0.0.0.0', port=port)
